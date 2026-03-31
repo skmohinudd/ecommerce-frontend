475201392fd8dbd1c15c9be1f01bec09e199e900
Khaja Mohinuddin <skkhajamohinuddin151@gmail.com>
2026-03-31 16:08:47 +0530

    deployment file placeholders are changed for images

--- src/api.js
+++ src/api.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL || "/api";
 
 export const getOrders = async () => {
-  const response = await axios.get(`${API_URL}/api/orders`);
+  const response = await axios.get(`${API_URL}/orders`);
   return response.data;
 };
 
 export const createOrder = async (orderData) => {
-  const response = await axios.post(`${API_URL}/api/orders`, orderData);
+  const response = await axios.post(`${API_URL}/orders`, orderData);
   return response.data;
 };
